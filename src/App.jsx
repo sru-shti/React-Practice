3b3e13f00d9c97ab733efbc4fc5f37655cc6b74c
Srushti <chobhesrushti@gmail.com>
2025-10-09 06:58:31 +0000

    added the button hooks

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,23 @@ function App() {
   )
 }
 function LightBulb(){
+ return <div>
+    <BulbState/>
+    <ToggleBulbState/>
 
+  </div>
 }
 
 function BulbState(){ //return img bulb on/off
- 
+ const [bulbOn, setBulbOn] = useState(true);
+   return <div>
+    { bulbOn ? "bulb on" : "bulb off"}
+   </div>
 }
 function ToggleBulbState(){
-
+return <div>
+    <button>Toggle the bulb</button>
+  </div>
 }
 export default App
 
