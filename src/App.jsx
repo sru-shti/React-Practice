c72d99d1bb1a5a98379abcd352267fd221b6584d
Srushti <chobhesrushti@gmail.com>
2025-10-09 06:54:02 +0000

    Initialise components

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  return ( <div>
+    <LightBulb/>
+  </div>
+
+  )
+}
+function LightBulb(){
+
+}
+
+function BulbState(){ //return img bulb on/off
+ 
+}
+function ToggleBulbState(){
+
+}
+export default App
+
+
